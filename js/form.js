51002293b33747705fc44fcd56bebe0629440c68
tatiana0203 <putchenko.t.a@gmail.com>
2021-02-11 15:15:22 +0200

    Validation and form design

--- js/form.js
+++ js/form.js
@@ -17,18 +17,21 @@ $('form').submit(function (e) {
 });
 
 
-
-
-/*
-
-
 $(function () {
+
   var name_input = $("#name_input");
   var name_line = $("#name_line");
   var phone_input = $("#phone_input");
   var phone_line = $("#phone_line");
   var comment_input = $("#comment_input");
   var comment_line = $("#comment_line");
+  var error_message_name=$("#error_message_name");
+  var error_message_phone=$("#error_message_phone");
+  var button_send=$("#button_send");
+  
+  error_message_name.hide();
+  error_message_phone.hide();
+
   //Functions for lines in form 
   name_input.focus(function () {
     name_line.css({ "border-width": "3px" });
@@ -59,28 +62,55 @@ $(function () {
 
     if (name_length < 2 || name_length > 100 || !name) {
       name_line.css({ "border-color": "red" });
+      error_message_name.show();
+      button_send.attr("disabled", "disabled");
+      return false;
     } else {
       name_line.css({ "border-color": "white" });
+      error_message_name.hide();
+      button_send.removeAttr("disabled", "disabled");
+      return true;
     };
   };
 
   //Validation for email or phone
   function check_email_phone() {
-    var patternEmail = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
-    var patternPhone = new RegExp(/\d/g);
+    var patternEmail = new RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i);
+    var patternPhone = new RegExp(/((\+)?\b(38)?(0[\d-]{2}))([\d-]{5,8})([\d-]{2})/);
     let email = patternEmail.test(phone_input.val());
     let phone = patternPhone.test(phone_input.val());
 
     if (phone) {
       phone_line.css({ "border-color": "white" });
+      error_message_phone.hide();
+      button_send.removeAttr("disabled", "disabled");
+      return true;
     } else if (email) {
       phone_line.css({ "border-color": "white" });
+      error_message_phone.hide();
+      button_send.removeAttr("disabled", "disabled");
+      return true;
     } else {
       phone_line.css({ "border-color": "red" });
+      error_message_phone.show();
+      button_send.attr("disabled", "disabled");
+      return false;
     };
   };
+
+  var textarea = document.getElementsByTagName('textarea')[0];
+
+  textarea.addEventListener('keydown', resize);
+  
+  function resize() {
+    var el = this;
+    setTimeout(function() {
+      el.style.cssText = 'height:23px; padding:0';
+      el.style.cssText = 'height:' + el.scrollHeight + 'px';
+    }, 1);
+  }
+
 
 });//end
 
 
-*/
