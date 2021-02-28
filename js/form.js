4850d309ca311ad9a05767e4611730219bf8e62e
Denis Scholokov <scholokov@users.noreply.github.com>
2021-02-28 21:40:32 +0200

    telegram

--- js/form.js
+++ js/form.js
@@ -12,7 +12,7 @@ $('form').submit(function (e) {
   var sum_text = 'Hi!\nI`m QAX Camp bot.\n' +
     'Please read message below.\n' +
     'Somebody wait your answer!\n\n' +
-    '\nBrowser: ' + platform.name + " v" + platform.version +
+    '\nBrowser: ' + platform.name + ' v' + platform.version +
     '\nOS: ' + platform.os +
     'Name: ' + $('#name_input').val() +
     '\nPhone: ' + $('#phone_input').val() +
