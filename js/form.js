33e875449b06964d58afd91eeabf4ff9605a7d21
Denis Scholokov <scholokov@users.noreply.github.com>
2021-03-10 16:37:24 +0200

    form

--- js/form.js
+++ js/form.js
@@ -14,16 +14,15 @@ $('form').submit(function (e) {
   '\nBrowser: ' + platform.name + ' v' + platform.version +
   '\nOS: ' + platform.os + */
 
-  var sum_text = 'Hi!\nI`m QAX Camp bot.\n' +
-    'Enviroment: ' + envName +
-    '\nPlease read message below.\n' +
-    'Somebody wait your answer!\n\n' +
-    'Name: ' + $('#name_input').val() +
-    '\nPhone: ' + $('#phone_input').val() +
-    '\nComment: ' + comment;   /* +  
-    '\nBrowser: ' + platform.name + " v" + platform.version +
-    '\nOS: ' + platform.os;
-    */
+  var sum_text = 'Hi!\nI`m QAX bot.\n' +
+  'Enviroment: ' + envName +
+  '\n\n' +
+  'Name: ' + $('#name_input').val() +
+  '\nPhone: ' + $('#phone_input').val() +
+  '\nComment: ' + comment;   /* +  
+  '\nBrowser: ' + platform.name + " v" + platform.version +
+  '\nOS: ' + platform.os;
+  */
 
   e.preventDefault();
   $.ajax({
