5b9c769c2e56b7f0db31a989f6e4ad321bd2368d
tatiana0203 <putchenko.t.a@gmail.com>
2021-03-21 20:01:38 +0200

    links test2

--- js/form.js
+++ js/form.js
@@ -43,7 +43,8 @@ $('form').submit(function (e) {
         'Name: ' + $('#name_input').val() +
         '\nPhone: ' + $('#phone_input').val() +
         '\nEmail: ' + $('#email_input').val() +
-        '\nComment: ' + comment;  /* +  
+        '\nComment: ' + comment; 
+        '\nLink:' + location.href; /* +  
       '\nBrowser: ' + platform.name + " v" + platform.version +
       '\nOS: ' + platform.os;
       */
