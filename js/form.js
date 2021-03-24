5c3b928ae612fb8e978dd632a1ca18d87912610f
tatiana0203 <putchenko.t.a@gmail.com>
2021-03-24 22:10:49 +0200

    email validation

--- js/form.js
+++ js/form.js
@@ -336,7 +336,7 @@ function check_email() {
     console.log('email-length: ' + email_length);
     console.log('email-correctness: ' + email_clear);
 
-    if (email_length > 50) {
+    } else if ((email_length !=0) && (email_length > 50)) {
         email_line.css({ "border-color": "red" });
         error_message_email_empty.hide();
         error_message_email_invalid.hide();
