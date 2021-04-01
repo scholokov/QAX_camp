f668b234ad3c3f20b006d7d9511d977d7cf2d479
tatiana0203 <putchenko.t.a@gmail.com>
2021-04-01 14:36:22 +0300

    email field

--- js/form.js
+++ js/form.js
@@ -5,7 +5,7 @@ $('form').submit(function (e) {
     envName = sessionStorage.getItem('envName');
 
     var comment = document.getElementById("comment_input").innerText;
-    var email = document.getElementById("email_input").innerText;
+    var email = document.getElementById("email_input").val();
     var userUrl=location.hash.slice(1);
     var result = true;
 
