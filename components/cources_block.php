37aba827e118e0577bc9d177ab74e81f080e0cf2
Denis Scholokov <scholokov@users.noreply.github.com>
2021-04-10 20:22:29 +0300

    cources - lot of changes

--- components/cources_block.php
+++ components/cources_block.php
@@ -1,65 +1,64 @@
 <div class="cources_details" id="cources_details">
 	<div id="cources_details-menu-block" class="row col-mb-50 mb-1">
 
-
 		<div class="col-lg-3">
 			<aside class="sidebar">
 
 				<div id="leftside-navigation">
 					<div class="accordion">
 						<div class="accordion__wrapper">
+			
+<?php
+$cource_tab_GET = $_GET["cource"] ; 
+$cource_name_GET = $_GET["name"] ; 
 
-							<div class="accordion__item">
-								<div class="menu-item-QA accordion__question" id="defaultOpen">
-									<!-- id="defaultOpen" -->
-									QA Engineer
-								</div>
-								<div class="accordion__answer">
-									<ul class="sub-menu tab active">
-										<li class="menu-sub-item-QA"><a href='#QA_automation'
-												onclick="openTab(event, 'qa_automation')" class="tablinks active"
-												id="defaultOpen">Автоматизація</a>
-										</li>
+	
+$json = file_get_contents( 'cources/_menu_root.json');
+$json_data = json_decode($json,true);
+foreach ($json_data as $cource_file => $cource_tab) {
+	if ($cource_tab['availability'])
+		{
+			if ( $cource_tab['cource'] ==  $cource_tab_GET )
+				$defaultFlag = 'id="defaultOpen"';
+			else
+				$defaultFlag = '';
+			
+			
+			
+			
+			echo '<div class="accordion__item">';
+			echo '<div class="menu-item-QA accordion__question" ' . $defaultFlag . '>';
+			echo $cource_tab['courceTabName'];
+			echo '</div>';
+			echo '<div class="accordion__answer">';
+			echo '<ul class="sub-menu tab active">';
 
-										<li class="menu-sub-item-QA"><a href='#QA_theory'
-												onclick="openTab(event, 'qa_theory')" class="tablinks">Менторство</a>
-										</li>
-										<li class="menu-sub-item-QA "><a href='#QA_practice'
-												onclick="openTab(event, 'qa_practice')" class="tablinks">Практика</a>
-										</li>
-									</ul>
-								</div>
-							</div>
+			
+			$json_cource = file_get_contents( 'cources/' . $cource_tab['courcefile'] );
+			$json_cource_data = json_decode($json_cource,true);
+			foreach ($json_cource_data as $cource_name_file => $cource_name)
+				{
+					if ($cource_name['availability']){
+						if ( $cource_name['courseFileName'] ==  $cource_name_GET ){
+							$activeFlag = 'active';
+							$defaultFlag = 'id="defaultOpen"';}
+						else{
+							$activeFlag = '';
+							$defaultFlag = '';}
 
-							<div class="accordion__item">
-								<div class="menu-item-QA accordion__question">
-									Business Analyst
-								</div>
-								<div class="accordion__answer">
-									<ul class="sub-menu tab">
-
-										<li class="menu-sub-item-QA"><a href='#ba_practice'
-												onclick="openTab(event, 'ba_practice')" class="tablinks">Практика</a>
-										</li>
-
-									</ul>
-								</div>
-							</div>
-
-							<div class="accordion__item">
-								<div class="menu-item-QA accordion__question">
-									Project Manager
-								</div>
-								<div class="accordion__answer">
-									<ul class="sub-menu tab">
-
-										<li class="menu-sub-item-QA"><a href='#pm_practice'
-												onclick="openTab(event, 'pm_practice')" class="tablinks">Практика</a>
-										</li>
-
-									</ul>
-								</div>
-							</div>
+						
+						echo '<li class="menu-sub-item-QA"><a href="#' . $cource_name['courseFileName'] . '"';
+							echo 'onclick="openTab(event, &#039;' . $cource_name['courseFileName'] . '&#039;)" class="tablinks ' . $activeFlag . '"';
+							echo $defaultFlag . '>' . $cource_name['courseName'] . '</a>';
+						echo '</li>';
+					}
+				}
+			echo '</ul>';
+				echo '</div>';
+			echo '</div>';
+		}
+}
+?>
 						</div>
 					</div>
 				</div>
@@ -70,9 +69,9 @@
 		<div class="col-xl-9">
 			<div class="cources_details-text">
 
-				<?php include 'cources/automation.html'?>
-				<?php include 'cources/practice-theory.html'?>
-				<?php include 'cources/practice.html'?>
+				<?php include 'cources/qa_automation.html'?>
+				<?php include 'cources/qa_theory.html'?>
+				<?php include 'cources/qa_practice.html'?>
 				<?php include 'cources/ba_practice.html'?>
 				<?php include 'cources/pm_practice.html'?>
 
@@ -80,4 +79,4 @@
 		</div>
 
 	</div>
-</div>
+</div>	
