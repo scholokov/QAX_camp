c8a34d39a585815f3bcee61a0b3843c3f3e5f0fc
tatiana0203 <putchenko.t.a@gmail.com>
2021-04-13 23:18:18 +0300

    mobile menu new design test

--- index.php
+++ index.php
@@ -95,12 +95,16 @@
 						<div class="bar2"></div>
 						<div class="bar3"></div>
 				</div>
+
 				
 			</div>
-		</header><!-- #header end -->
-		<div id="mobile-menu-fullscreen">
+			
+				<div id="mobile-menu-fullscreen">
 				
-		</div>
+		    	</div>
+
+		</header><!-- #header end -->
+		
 		<div id="heroImage-block">
 			<!-- ============================================ -->
 			<div>
