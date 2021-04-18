9cdc91329b143189b3e4a98abb20d4ae7f29b6c6
Denis Scholokov <scholokov@users.noreply.github.com>
2021-04-18 18:12:42 +0300

    accordion

--- components/cources_block.php
+++ components/cources_block.php
@@ -48,7 +48,7 @@ foreach ($json_data as $cource_file => $cource_tab) {
 							$defaultFlag = '';}
 
 						//echo '<li class="menu-sub-item-QA"><a href="#' . $cource_name['courseFileName'] . '"';
-						echo '<li class="menu-sub-item-QA"><a href="?cource=' . $cource_tab_id . '&name=' . $cource_name['courseFileName'] . '#1"';
+						echo '<li class="menu-sub-item-QA"><a href="?cource=' . $cource_tab_id . '&name=' . $cource_name['courseFileName'] . '"';
 							echo 'onclick="openTab(event, &#039;' . $cource_name['courseFileName'] . '&#039;)" class="tablinks ' . $activeFlag . '"';
 							echo $defaultFlag . '>' . $cource_name['courseName'] . '</a>';
 						echo '</li>';
