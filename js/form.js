db6d0e4982b626a8d58e9a8f25e94d4ea8050ae9
tatiana0203 <putchenko.t.a@gmail.com>
2021-04-18 04:13:16 +0300

    comment counter

--- js/form.js
+++ js/form.js
@@ -469,5 +469,5 @@ var maskOptions = {
 var mask = IMask(element, maskOptions);
 
 $("#comment_input").keyup(function() {
-    $("#comment-counter").text(this.value.length);
+    $("#comment-counter").text($("#comment_input").value.length);
 });
