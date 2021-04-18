2b27d05620543d95b1fe7075e0f2f6884a4156d0
Denis Scholokov <scholokov@users.noreply.github.com>
2021-04-18 20:51:52 +0300

    accordion test

--- cources.php
+++ cources.php
@@ -225,7 +225,17 @@
 		  
 			var nextTitle = 'ddddd';
 			var nextURL = 'ddd';
+			
+			// Current URL: https://my-website.com/page_a
+			const nextURL = 'https://my-website.com/page_b';
+			const nextTitle = 'My new page title';
+			const nextState = { additionalInformation: 'Updated the URL with JS' };
+
+			// This will create a new entry in the browser's history, without reloading
 			window.history.pushState(nextState, nextTitle, nextURL);
+
+			// This will replace the current entry in the browser's history, without reloading
+			window.history.replaceState(nextState, nextTitle, nextURL);
 
 		};
 		
