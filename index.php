6e5c8a5463b73349d31260a1fafd36812df24acd
tatiana0203 <putchenko.t.a@gmail.com>
2021-05-19 00:41:28 +0300

    intelinput ip

--- index.php
+++ index.php
@@ -547,10 +547,10 @@ function goToForm(){
 	============================================= -->
 	<!-- for phone mask -->
 	<script src="https://unpkg.com/imask"></script>
-	<script src="js/form.js"></script>
+	
 	<script src="js/intlTelInput.min.js"></script>
 	<script src="js/intlTelInput-jquery.min.js"></script>
-
+	<script src="js/form.js"></script>
 	<script src="js/functions.js"></script>
 	
 
