5878863a1faef9c862a02aa86515a86a698f028b
Tatiana Putchenko <putchenko.t.a@gmail.com>
2021-06-22 21:16:32 +0300

    name form

--- js/form.js
+++ js/form.js
@@ -331,7 +331,7 @@ function check_name() {
     var name_clear = name_input.val().trim();
 
     var name_length = name_clear.length;
-var patternName = new RegExp(/^[a-zA-Zа-яА-яЁёàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ðіїІ `'-\u{0060}\u{2018}\u{2019}\u{2032}\u{0027}]$/ui);
+var patternName = new RegExp(/^[a-zA-Zа-яА-яЁёàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ðіїІ `'-\u{0060}\u{2018}\u{2019}\u{2032}\u{0027}]/ui);
     let name = patternName.test(name_clear);
 
     console.log('name-length: ' + name_length);
