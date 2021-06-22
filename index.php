419aa6aaf1a4c3c1f3e8cb2cf860bd5e44e4116b
Tatiana Putchenko <putchenko.t.a@gmail.com>
2021-06-22 22:00:51 +0300

    name form

--- index.php
+++ index.php
@@ -452,7 +452,6 @@
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
-	<script src="https://code.jquery.com/jquery-latest.min.js">
 	<script src="js/plugins.min.js"></script>
 
 
