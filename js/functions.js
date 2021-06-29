0d57099861f3ae963771ca0730bde87639356117
Tatiana Putchenko <putchenko.t.a@gmail.com>
2021-06-29 13:38:14 +0300

    header

--- js/functions.js
+++ js/functions.js
@@ -17,7 +17,18 @@ window.onload=function(){
 	telegram_link.mouseout(function(){telegram_img.attr({"src":"images/Telegram.svg"})});
   };
   
-
+$(function () {
+    var location = window.location.href;
+    var cur_url = '/' + location.split('/').pop();
+ 
+    $('.menu li').each(function () {
+        var link = $(this).find('a').attr('href');
+ 
+        if (cur_url == link) {
+            $(this).addClass('current');
+        }
+    });
+});
 // Scrolled
 $.fn.scrollEnd = function(callback, timeout) {
 	$(this).scroll(function(){
