e0ae42d723fc1ea9b4aa9297c2a79b94cdd2cde8
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2021-07-12 20:38:40 +0300

    form phone

--- js/form.js
+++ js/form.js
@@ -589,7 +589,7 @@ $("#phone_input").intlTelInput({
     
     placeholderNumberType:"MOBILE",
     
-    preferredCountries: null,
+    preferredCountries: [],
     
     separateDialCode:false,
     
