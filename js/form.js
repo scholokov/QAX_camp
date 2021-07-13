aef47dace1b6b2f4e2a63d6fbd6b6a271a899ea6
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2021-07-13 19:28:48 +0300

    form

--- js/form.js
+++ js/form.js
@@ -647,7 +647,7 @@ window.intlTelInput(input, {
   initialCountry: "auto",
   geoIpLookup: function(callback) {
     $.get('https://ipinfo.io').always(function(resp) {
-      var countryCode = (resp && resp.country) ? resp.country : "ua";
+      var countryCode = (resp && resp.country) ? resp.country : "";
       callback(countryCode);
     });
   },
