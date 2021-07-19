431b06c446a99b3b2855945a246299f80d10b561
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2021-07-19 14:29:09 +0300

    Olesya

--- our_trainers.php
+++ our_trainers.php
@@ -177,7 +177,7 @@
                             <div class="trainer-info row">
                                 <div class="col-9">
                                 <div class="trainer-name">Олеся Ломенко</div>
-                                <div class="trainer-post">Mentor for PMs and BAs</div>
+                                <div class="trainer-post">Mentor</div>
                                 </div>
                                 <div class="linkedin-logo col-3">
                                     <div><a href="https://www.linkedin.com/in/alesya-lomenko/">
