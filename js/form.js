c9b202698b3b206110f941bce8615ac97f720391
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2021-07-20 13:55:41 +0300

    form ip

--- js/form.js
+++ js/form.js
@@ -644,6 +644,7 @@ $("#phone_input").intlTelInput("loadUtils", "js/utils.js");
 
 var input = document.querySelector("#phone_input");
 window.intlTelInput(input, {
+  initialCountry: "auto",
   geoIpLookup: function(callback) {
     $.get('https://ipinfo.io',function () { }, "jsonp").always(function(resp) {
       var countryCode = (resp && resp.country) ? resp.country : "";
