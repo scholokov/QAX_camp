3d33c6952982c5563c1ef8f4a39cd7f986d345bc
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2021-08-05 23:50:19 +0300

    dots

--- index.php
+++ index.php
@@ -397,102 +397,99 @@
 							</div>
 
 							<div class="feedbackSlides">
-								<h3 class="feedback-author">Andrey Hryhorchuk</h3>
-								<h5 class="feedback-course">Курс Quality Assurance Engineer Practice</h5>
-								<h4 class="feedback-text">Привет! меня зовут Андрей, хочу оставить свой отзыв о компании
-									QAX. В данном компании мне понравилось то что не "бросают на произвол судьбы", а
-									объясняют "что, как, почему, когда и где". Данная компания может служить подробным
-									мануалом как должны быть налажены рабочие процессы :) Коллектив приятный, ощущается
-									эффект уюта и домашнего комфорта во время митингов :)</h4>
+								<div id="success-cases-block">
+										<div id="success-cases-duration">3 місяці</div>
+										<div id="success-cases-arrow">
+											<img src="images/Arrow_success.svg" alt=arrow>
+										</div>
+										<div id="success-cases-info">
+											<div id="success-cases-info-block1">
+												<div class="feedback-author">Alexander Bezeliuk</div>
+												<div class="feedback-job">QA engineer (intern)</div>
+												<div class="feedback-text">Playtech</div>
+											</div>
+											<div id="success-cases-info-block2">
+												<div class="feedback-text">QA Manual</div>
+												<div class="feedback-text">QAX Camp</div>
+											</div>
+											<div id="success-cases-info-block3">
+												<div class="feedback-text">Заступник директору магазину елекроніки </div>
+												<div class="feedback-text">3 year of overall NON-IT Experiance</div>
+											</div>
+										</div>
+									</div>
 							</div>
 
 							<div class="feedbackSlides">
-								<h3 class="feedback-author">Dmytro Tytarenko</h3>
-								<h5 class="feedback-course">Курс Quality Assurance Engineer Practice</h5>
-								<h4 class="feedback-text">QAX camp! Откровенно говоря, это то что нужно всем начинающим(
-									и не только начинающим) QA!<br>
-
-									-Курсы курсами, но реальный проект-это действительно,то что нужно выпускникам после
-									теории и «практики».<br>
-
-									-Работа согласно всем процессам тестирования. Это то чего не хватает в багаже
-									знаний. Теорию выучили, а
-									процессов никто не знает, здесь же все присутствует.<br>
-
-									-Работа в команде! Даже если кто-то где-то в каком-то моменте не отстреливает ни
-									грамма, команда и
-									кураторы/наставники/менторы всегда объяснят доходчиво и понятно.<br>
-
-									Моя ситуация была такова: Я учился с ментором, теорию выучил, с документацией
-									попрактиковался, остаётся вопрос
-									«Как все это происходит в реальном проекте?». Я попал в QAX camp и что вы думаете? В
-									реальном проекте с
-									поддержкой наставников и командой въехать во все тонкости процесса было намного
-									легче нежели сидеть смотреть
-									видосы на ютубе или читать сотни форумов!<br>
-
-									В общем, всего не расписать, нужно записываться, приходить и получать необходимый
-									опыт как теоретический так и
-									практический!
-									Рекомендую QAX camp!</h4>
+							<div id="success-cases-block">
+										<div id="success-cases-duration">1,5 місяці</div>
+										<div id="success-cases-arrow">
+											<img src="images/Arrow_success.svg" alt=arrow>
+										</div>
+										<div id="success-cases-info">
+											<div id="success-cases-info-block1">
+												<div class="feedback-author">Pavel Karnasenko</div>
+												<div class="feedback-job">QA Engineer</div>
+												<div class="feedback-text">Playtech</div>
+											</div>
+											<div id="success-cases-info-block2">
+												<div class="feedback-text">QA Manual</div>
+												<div class="feedback-text">QAX Camp</div>
+											</div>
+											<div id="success-cases-info-block3">
+												<div class="feedback-text">Інженер-конструктор</div>
+												<div class="feedback-text">4 year of overall NON-IT Experiance</div>
+											</div>
+										</div>
+									</div>
 							</div>
 
 							<div class="feedbackSlides">
-								<h3 class="feedback-author">Yulia Shelton</h3>
-								<h5 class="feedback-course">Курс Quality Assurance Engineer Practice</h5>
-								<h4 class="feedback-text">Очень ценными были практические знания в работе с
-									инструментами Test Management for Jira, анализе требований к проекту, коммуникации
-									со всеми членами команды. Отдельная благодарность Денису за очень детальный фидбек
-									по всем заданиям! Так держать!</h4>
+							<div id="success-cases-block">
+										<div id="success-cases-duration">5 місяці</div>
+										<div id="success-cases-arrow">
+											<img src="images/Arrow_success.svg" alt=arrow>
+										</div>
+										<div id="success-cases-info">
+											<div id="success-cases-info-block1">
+												<div class="feedback-author">Natali Sorokina</div>
+												<div class="feedback-job">Junior QA</div>
+												<div class="feedback-text">FireKass</div>
+											</div>
+											<div id="success-cases-info-block2">
+												<div class="feedback-text">QA Manual</div>
+												<div class="feedback-text">QAX Camp</div>
+											</div>
+											<div id="success-cases-info-block3">
+												<div class="feedback-text">Керуючий банківським відділенням</div>
+												<div class="feedback-text">22 year of overall NON-IT Experiance</div>
+											</div>
+										</div>
+									</div>
 							</div>
 
 							<div class="feedbackSlides">
-								<h3 class="feedback-author">Юрий Звир</h3>
-								<h5 class="feedback-course">Курс Quality Assurance Engineer Practice</h5>
-								<h4 class="feedback-text">Что мне нравиться в QAX.
-									За время работы доводилось иметь дело как с аутсорсными проектами, так и со своим
-									продуктом. Кроме собственно работы моделируются различные рабочие ситуации. Причём
-									это происходит по типу: "Как правильно. Как должно быть в идеале. Как это
-									происходило на моем опыте. Как, зачастую, бывает на практике и что с этим делать".
-
-									Регулярные продуктовые встречи, тестовые собеседования как с рекрутером, так и с
-									тимлидом, где вы получаете фитбек и разборы полетов от спецалистов с разным
-									беграундом, подходом и точкой зрения. Вы можете задавать любые вопросы по процессам,
-									технологиям, инструментам взаимодействию в командах и т. д. - и получаете на них
-									качественные развернутые ответы с примерами на реальной практике. Если у вас пробелы
-									по какой-то конкретной теме и это будет полезно для большинства, то вполне обычная
-									практика, что вам еще и прочитают лекцию на эту тему, дадут ссылки на полезные
-									ресурсы ну и, будте готовы, затем допросят с пристрастием).
-									Кроме групповых встреч есть индивидуальные созвоны, где идет проработка по вашему
-									кейсу и где вы можете обсудить вопросы, которые не хотели озвучить в группе.
-
-									Также - сбор фитбека от студентов. Учитывается критика и пожелания, вплоть до того,
-									что если всем хотелось бы поработать с какими-то инструментами или технологиями то
-									ребята стараются по возможности включать эти вещи в процесс.
-
-									Ну и пару слов о доброжелательных людях, которые стараются сделать практику
-									максимально продуктивной.
-									У Дениса 15+ лет опыта работы в сфере QA, огромные знания и опыт по технологиям,
-									процессам, формированию и управлению командами и т. д., тысячи собеседований с
-									соискателями.
-									Андрей, имеющий обширные знания в различных сферах IT, большой опыт работы по поиску
-									и найму специалистов разных направлений, который сам ходит на собеседования, чтобы
-									держать руку на пульсе и лучше понимать, что и как чувствует человек проходящий
-									собеседование.
-									Ребята, большое спасибо за вашу работу.</h4>
-							</div>
-
-							<div class="feedbackSlides">
-								<h3 class="feedback-author">Max Lapaichuk</h3>
-								<h5 class="feedback-course">Курс Quality Assurance Engineer Practice</h5>
-								<h4 class="feedback-text">Хочу подякувати компанії QAX, вони розробили шикарний курс
-									"Отримання досвіду на реальних проектах + менторство" на якому ти береш участь у
-									розробці проекту і впливаєш на кінцевий результат вносячи свої пропозиції щодо
-									покращення. QAX - це команда професіоналів з багатолітнім досвідом які готові
-									допомогти тобі 24/7. Особливо хочу подякувати Денису за його підтримку і роз'яснення
-									всіх тонкостей роботи QA, а також Андрію за мотивацію і допомогу з пошуком роботи.
-									Якщо ви хочете здобути досвід для працевлаштування, я РЕКОМЕНДУЮ записатися на курс.
-								</h4>
+							<div id="success-cases-block">
+										<div id="success-cases-duration">3 місяці</div>
+										<div id="success-cases-arrow">
+											<img src="images/Arrow_success.svg" alt=arrow>
+										</div>
+										<div id="success-cases-info">
+											<div id="success-cases-info-block1">
+												<div class="feedback-author">Max Lapaichuk</div>
+												<div class="feedback-job">Manual QA</div>
+												<div class="feedback-text">Leeloo</div>
+											</div>
+											<div id="success-cases-info-block2">
+												<div class="feedback-text">QA Manual</div>
+												<div class="feedback-text">QAX Camp</div>
+											</div>
+											<div id="success-cases-info-block3">
+												<div class="feedback-text">Офіціант - адміністратор</div>
+												<div class="feedback-text">4 year of overall NON-IT Experiance</div>
+											</div>
+										</div>
+									</div>
 							</div>
 
 							<ul id="dots">   
