a600dcb16927df99d65ab7640407c8466d511401
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2021-08-20 13:26:48 +0300

    slider

--- index.php
+++ index.php
@@ -516,9 +516,9 @@
 							<a class="next" onclick="plusSlides(1)"><img src="images/Arrow_right.svg"></a>
 
 						</div>
-						
-						<div class="slick-slider-mob col-md-10 offset-md-1">
 						<img src="images/Arrow_success.svg" alt=arrow class="success-cases-arrow-mob">
+						<div class="slick-slider-mob col-md-10 offset-md-1">
+						
 							<div class="success-cases-block" >
 								<div id="success-cases-duration">3 місяці</div>
 								
