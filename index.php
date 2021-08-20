ee52c9bc6bd84ae09ea31594a86fde03f6ff9c74
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2021-08-20 13:09:59 +0300

    slider

--- index.php
+++ index.php
@@ -517,11 +517,12 @@
 
 						</div>
 						<div class="slick-slider-mob col-md-10 offset-md-1">
+							<div id="success-cases-arrow">
+										<img src="images/Arrow_success.svg" alt=arrow>
+							</div>
 							<div class="success-cases-block" >
 								<div id="success-cases-duration">3 місяці</div>
-								<div id="success-cases-arrow">
-										<img src="images/Arrow_success.svg" alt=arrow>
-								</div>
+								
 										<div id="success-cases-info">
 											<div id="success-cases-info-block1">
 												<div class="feedback-author">Max Lapaichuk
