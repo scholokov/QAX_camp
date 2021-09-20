4c51709033f958c5d3576cb9f6a2b22e26fecd67
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2021-09-20 12:17:44 +0300

    table

--- our_trainers.php
+++ our_trainers.php
@@ -222,8 +222,9 @@
 									<p class="trainer-experience-title">Досвід викладацької діяльності:</p>
 									<p class="trainer-experience">
 									<table id="trainer-experience-table">
-									<tr><td>2021</td> <td>-</td>  <td>зараз</td>  <td>–</td> <td>Trainer в QAX Camp</td></tr></p>
+									<tr><td>2021</td> <td>-</td>  <td>зараз</td>  <td>–</td> <td>Trainer в QAX Camp</td></tr>
 									</table>
+								</p>
 									</p>
 									</div>
                             </div>
