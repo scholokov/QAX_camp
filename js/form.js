1bfe7e9b9f030fd7b8c49dc93c5ac88df9cdfa19
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2021-10-26 12:19:07 +0300

    form mobile number

--- js/form.js
+++ js/form.js
@@ -640,6 +640,9 @@ $$("#phone_input").intlTelInput("selectCountry", "");
 
 // Вставить номер и, соответственно, обновить выбранный флаг.
 //$("#demo").intlTelInput("setNumber", "+44 7733 123 456");
+var countryData = window.intlTelInputGlobals.getCountryData(),
+  input = document.querySelector("#phone_input"),
+  addressDropdown = document.querySelector("#address-country");
 for (var i = 0; i < countryData.length; i++) {
     var country = countryData[i];
     var optionNode = document.createElement("option");
