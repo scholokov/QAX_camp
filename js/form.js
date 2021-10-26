744dd2f9311e46262b509679d7f41be1a9837db9
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2021-10-26 11:04:21 +0300

    form mobile number

--- js/form.js
+++ js/form.js
@@ -616,7 +616,7 @@ $("#phone_input").intlTelInput({
 });*/
 
 // Получить код страны
-var extension = $("#phone_input").intlTelInput("getExtension");
+//var extension = $("#phone_input").intlTelInput("getExtension");
 
 // Получить текущий номер в данном формате
 var intlNumber = $("#phone_input").intlTelInput("getNumber");
