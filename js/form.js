d681eae856d4e9f212aceaed2b8fd9d694ee4175
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2021-10-26 14:03:39 +0300

    form mobile number

--- js/form.js
+++ js/form.js
@@ -640,28 +640,28 @@ $("#phone_input").intlTelInput("selectCountry", "");
 
 // Вставить номер и, соответственно, обновить выбранный флаг.
 //$("#demo").intlTelInput("setNumber", "+44 7733 123 456");
-/*var countryData = window.intlTelInputGlobals.getCountryData(),
-  input = document.querySelector("#phone"),
-  addressDropdown = document.querySelector("#address-country");
+var countryData = window.intlTelInputGlobals.getCountryData(),
+  input = document.querySelector("#phone_input"),
+  /*addressDropdown = document.querySelector("#address-country");*/
 for (var i = 0; i < countryData.length; i++) {
     var country = countryData[i];
     var optionNode = document.createElement("option");
     optionNode.value = country.iso2;
     var textNode = document.createTextNode(country.name);
     optionNode.appendChild(textNode);
-    addressDropdown.appendChild(optionNode);
+    /*addressDropdown.appendChild(optionNode);*/
   }
-  addressDropdown.value = iti.getSelectedCountryData().iso2;
-*/
+ /*addressDropdown.value = iti.getSelectedCountryData().iso2;*/
+
 // listen to the telephone input for changes
-$("#phone_input").on('countrychange', function(e) {
+input.addEventListener('countrychange', function(e) {
   this.value = iti.getSelectedCountryData().iso2;
 });
 
 // listen to the address dropdown for changes
 /*addressDropdown.addEventListener('change', function() {
-  iti.setCountry(this.value);
-});*/
+  iti.setCountry(this.value);*/
+});
 console.log= function() {};
 var input = document.querySelector("#phone_input");
 window.intlTelInput(input, {
