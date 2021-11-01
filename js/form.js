fb49f89e2109cfdc4c40bea101cd4ff12d9a9b3a
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2021-11-01 14:48:21 +0200

    form mobile number

--- js/form.js
+++ js/form.js
@@ -636,7 +636,7 @@ var isValid = $("#phone_input").intlTelInput("isValidNumber");
 $("#phone_input").intlTelInput("loadUtils", "js/utils.js");
 
 // Изменить выбранную страну
-$("#phone_input").intlTelInput("selectCountry", "");
+$("#phone_input").intlTelInput("selectCountry", "")
 
 // Вставить номер и, соответственно, обновить выбранный флаг.
 //$("#demo").intlTelInput("setNumber", "+44 7733 123 456");
