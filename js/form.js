368bfcd7981ab6f65f980cab0c0a98a0afd34e1c
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2021-11-16 13:18:44 +0200

    form

--- js/form.js
+++ js/form.js
@@ -528,7 +528,7 @@ function check_phone() {
         error_message_phone_only_numbers.hide();
         return false;
     }*/
-    else if ($.trim(input.val())) {
+    else if ($.trim(phone_input.val())) {
           if (input.intlTelInput("isValidNumber")) {
             phone_line.css({ "border-color": "#212121" });
             error_message_phone_empty.hide();
