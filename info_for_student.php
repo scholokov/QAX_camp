b51575cade6e098a3a83dd262623561343465008
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2021-12-12 22:22:04 +0200

    new page

---
+++ info_for_student.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="uk">
+
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="SemiColonWeb" />
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link
+		href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Raleway:wght@400;500;700&display=swap"
+		rel="stylesheet">
+
+	<!-- Stylesheets
+	============================================= -->
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+
+	
+	<link rel="stylesheet" href="css/common.css" type="text/css" />
+	<link rel="stylesheet" href="css/block.cources.css" type="text/css" />
+	<link rel="stylesheet" href="css/block.feedback.css" type="text/css" />
+	<link rel="stylesheet" href="css/footer.css" type="text/css" />
+	<link rel="stylesheet" href="css/header.css" type="text/css" />
+	<link rel="stylesheet" href="css/block.hero-image.css" type="text/css" />
+	<link rel="stylesheet" href="css/block.why_we.css" type="text/css" />
+	<link rel="stylesheet" href="css/block.write_to_us.css" type="text/css" />
+	<link rel="stylesheet" href="css/block.cources_detailes.css" type="text/css" />
+	<link rel="stylesheet" href="css/block.cources_detailes.accordion.css" type="text/css" />
+	<link rel="stylesheet" href="css/block.about_us.css" type="text/css" />
+	<link rel="stylesheet" href="css/intlTelInput.css">
+	<link rel="stylesheet" href="css/block.privacy_policy.css" type="text/css" />
+
+	<!-- Document Title
+	============================================= -->
+	<title>QAX Camp - Privacy Policy</title>
+	<link rel="icon" type="image/png" href="favicon.png">
+
+	<script>
+		function readTextFile(file, callback) {
+			var rawFile = new XMLHttpRequest();
+			rawFile.overrideMimeType("application/json");
+			rawFile.open("GET", file, true);
+			rawFile.onreadystatechange = function () {
+				if (rawFile.readyState === 4 && rawFile.status == "200") {
+					callback(rawFile.responseText);
+				}
+			}
+			rawFile.send(null);
+		}
+
+		//usage:
+		readTextFile("env.json",
+			function (text) {
+				var data = JSON.parse(text);
+				/* console.log(data); */
+				sessionStorage.setItem('facebookLink', data.facebookLink);
+				sessionStorage.setItem('envName', data.envName);
+				sessionStorage.setItem('tokenID', data.telega.tokenID);
+				sessionStorage.setItem('chatID', data.telega.chatID);
+			});
+	</script>
+
+</head>
+
+<body class="stretched">
+
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+
+		<!-- Header
+		============================================= -->
+		<header>
+			<div class="header-menu" id="header-block">
+				<a href="index.php"><img src="images/Logo_Small.svg" alt="Logo" id="logo"></a>
+			</div>
+		</header><!-- #header end -->
+
+		<!-- Page Title
+		============================================= -->
+
+
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+			<div class="content-wrap" id="privacy_policy">
+				<div class="privacy_policy_main_block container clearfix">
+					<div id="privacy_policy_content">
+						
+					 <p>Конфіденційність користувачів нашого веб-сайту має велике значення для нас 
+						 і ми докладаємо всіх зусиль для забезпечення її захисту. 
+						 Дані правила пояснюють, як ми використовуємо вашу персональну інформацію.</p>
+
+                   
+					</div>
+					
+				</div>
+			
+			</div>
+		</section><!-- #content end -->
+
+		
+
+		<!-- Footer
+		============================================= -->
+		<?php include 'components/footer.html'?>
+
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-line-chevron-up"></div>
+
+	<!-- JavaScripts
+	============================================= -->
+	<script src="js/include.js"></script>
+
+	<!-- <script src="js/jquery.js"></script> -->
+
+
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+	<script src="js/plugins.min.js"></script>
+
+			
+
+	<!-- Footer Scripts
+	============================================= -->
+	<!-- for phone mask -->
+
+	<script src="js/intlTelInput.min.js"></script>
+	<script src="js/intlTelInput-jquery.min.js"></script>
+	<script src="js/functions.js"></script>
+
+</body>
+
+</html>
