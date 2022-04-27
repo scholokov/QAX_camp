225f45e9731e103664aa2b430f5b1a59a50d6ec4
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2022-04-27 07:12:55 -0600

    form stud

--- newby.php
+++ newby.php
@@ -81,7 +81,7 @@
 		============================================= -->
 
 	
-					<iframe src="https://forms.monday.com/forms/embed/7907437eb478113f6d5159033607ed0e?r=use1" width="650" height="900" style="border: 0; box-shadow: 5px 5px 56px 0px rgba(0,0,0,0.25);"></iframe>
+					<iframe src="https://forms.monday.com/forms/embed/7907437eb478113f6d5159033607ed0e?r=use1" width="650" style="border: 0; box-shadow: 5px 5px 56px 0px rgba(0,0,0,0.25);"></iframe>
                     
 
 
