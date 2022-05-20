c323ebed8059d5d23dc3be74354f63081f97c344
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2022-05-20 03:37:15 -0600

    new page

--- new_course.php
+++ new_course.php
@@ -118,7 +118,7 @@
 					<div id="new-course-start-block">
 						<div id="new-course-main-text">
 							<h3 id="new-course-main-title">Курс "Практичні інструменти"</h3>
-							<div class="new-courses-main-description">Майстри своєї справи які будуть разом 
+							<div class="new-course-main-description">Майстри своєї справи які будуть разом 
 								з тобою протягом усієї дороги навчання
 								 та радо дадуть ключі на усі твої запитання які ти так довго шукав сам.</div>
 						</div>
