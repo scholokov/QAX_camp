219fc32041291bee5ea2e58eb0b1b2a7ca7993f0
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2022-05-20 03:30:38 -0600

    new page

--- new_course.php
+++ new_course.php
@@ -114,11 +114,11 @@
 		============================================= -->
 		<section id="content">
 			<div class="content-wrap">
-				<div class="container clearfix trainers-main-block">
-					<div id="trainers-start-block">
-						<div id="trainers-main-text">
-							<h3 id="trainers-main-title">Наші тренери</h3>
-							<div class="trainers-main-description">Майстри своєї справи які будуть разом 
+				<div class="container clearfix new-course-main-block">
+					<div id="new-course-start-block">
+						<div id="new-course-main-text">
+							<h3 id="new-course-title">Курс "Практичні інструменти"</h3>
+							<div class="new-courses-main-description">Майстри своєї справи які будуть разом 
 								з тобою протягом усієї дороги навчання
 								 та радо дадуть ключі на усі твої запитання які ти так довго шукав сам.</div>
 						</div>
