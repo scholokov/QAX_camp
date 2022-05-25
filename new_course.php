8ecf9a1ff6a5db6116149cd677be39bce66fbed4
Tatiana Putchenko <75476149+tatiana0203@users.noreply.github.com>
2022-05-25 07:16:17 -0600

    new page

--- new_course.php
+++ new_course.php
@@ -374,7 +374,7 @@
                     </div>
 
 
-                    <iframe src="https://forms.monday.com/forms/69b9a1562e54e5c9222bb5c4b3bf902a?r=use1"  style="max-height: 1300px; height: 95%; border: 0; box-shadow: 5px 5px 56px 0px rgba(0,0,0,0.25);"></iframe>
+                    <iframe src="https://forms.monday.com/forms/embed/7907437eb478113f6d5159033607ed0e?r=use1"  style="max-height: 1300px; height: 95%; border: 0; box-shadow: 5px 5px 56px 0px rgba(0,0,0,0.25);"></iframe>
 
 
 
