473dea76ef8957a2024e4468d74acedf11d5ada6
Tatiana Putchenko <putchenko.t.a@gmail.com>
2022-06-23 16:34:32 +0300

    test

--- base_course.php
+++ base_course.php
@@ -8,11 +8,12 @@
 	<meta name="author" content="SemiColonWeb" />
 
 	<link rel="preconnect" href="https://fonts.gstatic.com">
-    
+
 	<link
 		href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Raleway:wght@400;500;700&display=swap"
 		rel="stylesheet">
 
+        
 	<!-- Stylesheets
 	============================================= -->
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
