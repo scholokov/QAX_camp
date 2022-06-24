137413fb8dd7654e2722ed755b1e31ecc39c5155
tatiana0203 <putchenko.t.a@gmail.com>
2022-06-24 15:04:40 +0300

    sql course link

--- base_course.php
+++ base_course.php
@@ -475,7 +475,7 @@
 
 
 				</div>
-               <iframe name="iframe1" id="iframe1" src="https://forms.monday.com/forms/embed/7907437eb478113f6d5159033607ed0e?r=use1"  style="min-height: 2100px; max-height: 2350px; height: 100%; border: 0; margin-bottom: -10px;"></iframe>
+               <iframe name="iframe1" id="iframe1" src="https://forms.monday.com/forms/embed/7907437eb478113f6d5159033607ed0e?r=use1"  style="min-height: 2050px; max-height: 2350px; height: 100%; border: 0; margin-bottom: -10px;"></iframe>
 			</div>
 		</section>
 
